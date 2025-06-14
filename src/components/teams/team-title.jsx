c542116050f3cf65ee4012bc5fba36b1a06aafa7
mshalom27 <menshalom07@gmail.com>
2025-06-14 22:58:30 +0400

    image and font size

--- src/components/teams/team-title.jsx
+++ src/components/teams/team-title.jsx
@@ -1,20 +1,25 @@
-
+import Image from 'next/image';
 export default function TeamTitle() {
     return (
     <div className="text-center w-full h-full mb-16" style={{ backgroundColor: '#BC82FE' }}>
 
-        <div className="relative w-full h-4 bg-[#BC82FE]">
-        <img
-        src="https://res.cloudinary.com/djqgs2pet/image/upload/v1749742278/circle-sticker-teams_ccxfdi.png"
-        alt="Sticker"
-        className="absolute h-auto m-0 p-0 top-0 right-0 z-10 w-20 sm:w-28 md:w-32 lg:w-36"
+       <div className="relative w-full h-0 m-0 p-0">
+      <div className="absolute top-0 right-0 z-10 w-20 sm:w-28 md:w-32 lg:w-36 h-auto">
+        <Image
+          src="https://res.cloudinary.com/djqgs2pet/image/upload/v1749742278/circle-sticker-teams_ccxfdi.png"
+          alt="Sticker"
+          layout="responsive"
+          width={100}
+          height={100}
+          priority
         />
-        </div>
+      </div>
+    </div>
 
-        <div className="relative  flex justify-center items-center">
+        <div className="relative pt-5 flex justify-center items-center">
           <div className="absolute top-1/2 left-0 right-0 h-0.5 bg-black -translate-y-1/2 z-0"></div>
 
-          <div className="bg-white rounded-lg px-4 py-3 border-2 border-black z-10">
+          <div className="bg-white rounded-xl px-4 py-3 border-2 border-black z-10">
           <div className="bg-white rounded-lg px-8 py-3 border-2 border-dashed border-black">
             <h1 className="text-xl md:text-5xl font-semibold font-clash-display text-black tracking-wider">
             OUR TEAMS
--- src/components/teams/teams.jsx
+++ src/components/teams/teams.jsx
@@ -4,8 +4,11 @@ import TeamTitle from './team-title';
 export default function Teams() {
   return (
     <div style={{ backgroundColor: '#BC82FE' }}>
+      
       <TeamTitle />
       <Stickers />
+      
     </div>
+    
   );
 }
