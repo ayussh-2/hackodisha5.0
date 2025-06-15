3ebf390839e258ecd2cafe9c831ef826fc0a6c4d
PLANETORIAN <daksh06shah2006@gmail.com>
2025-06-15 10:25:03 +0530

    fix

--- src/components/prizes/gold.jsx
+++ src/components/prizes/gold.jsx
@@ -3,43 +3,35 @@ import Image from "next/image"
 function Gold() {
   return (
     <div className="bg-[#D3AEFF] border-1 border-black relative md:rounded-2xl rounded-[100px] p-5 md:row-span-2 flex flex-col items-center justify-center overflow-visible">
-    
-              {/* Tag Image with Text */}
-              <div className="absolute hidden md:block xl:-top-31 -left-11 -top-31 xl:-left-10 w-55 xl:rotate-[0deg] z-10">
-      <Image
-        src="https://res.cloudinary.com/dbdkg7fik/image/upload/v1749798805/Asset_1_1_2_gkhzjv.svg"
+      <div className="absolute hidden md:block xl:-top-31 -left-11 -top-31 xl:-left-10 w-55 xl:rotate-[0deg] z-10">
+        <Image
+          src="https://res.cloudinary.com/dbdkg7fik/image/upload/v1749798805/Asset_1_1_2_gkhzjv.svg"
         alt="Cash Prize Tag"
-        width={112} // You can tweak this as needed (28 * 4 = 112px)
-        height={100} // Estimate height; adjust based on SVG shape
+        width={112} 
+        height={100} 
         className="w-[300px] scale-[1.3] xl:scale-[1.45] h-auto"
-      />
+        />
       <div className="absolute inset-0 flex flex-col items-center left-17 -top-1 justify-center rotate-[-16deg] px-2">
-      <span className="text-black font-extrabold font-clash-display text-xl text-center leading-tight">
-        CASH PRIZE
-      </span>
+        <span className="text-black font-extrabold font-clash-display text-xl text-center leading-tight">
+          CASH PRIZE
+        </span>
 
-      <span  style={{
-    WebkitTextStroke: '1px black',
-    color: '#FFEA89',
-  }}
-  className="text-[#FFEA89] font-extrabold font-clash-display text-3xl text-center  leading-tight">
-        ₹25k
-      </span>
-    </div>
+        <span  style={{
+          WebkitTextStroke: '1px black',
+          color: '#FFEA89',
+          }}
+          className="text-[#FFEA89] font-extrabold font-clash-display text-3xl text-center  leading-tight">
+          ₹25k
+        </span>
+      </div>
     
     </div>
     
-              {/* Card Content */}
+  
               <div className="bg-white w-full h-full rounded-[110px] md:rounded-2xl px-4 pt-3 sm:pt-10 md:pt-12 pb-12 md:pb-20 flex flex-col items-center justify-between relative overflow-visible">
-    
-                {/* Capsule with GOLD Text */}
-                {/* Capsule Wrapper */}
-   {/* Capsule with GOLD Text */}
 <div className="relative mb-10 flex items-center justify-center w-full">
-  {/* Yellow Capsule Container */}
   <div className="relative bg-[#FFCC33] rounded-full flex items-center justify-center sm:px-10 md:py-4 xl:py-5 min-w-[150px] sm:min-w-[220px] md:min-w-[300px] xl:min-w-[370px] sm:min-h-[70px] min-h-[50px] border border-black overflow-visible">
     
-    {/* Outline Image for Desktop (≥768px) */}
     <div
       className="absolute inset-1.5 items-center justify-center pointer-events-none  hidden md:flex"
       style={{ padding: '10px', boxSizing: 'border-box' }}
@@ -53,7 +45,7 @@ function Gold() {
       />
     </div>
 
-    {/* Outline Image for Mobile (<768px) */}
+
     <div
       className="absolute inset-1.5 flex items-center justify-center pointer-events-none md:hidden"
       style={{ padding: '6px', boxSizing: 'border-box' }}
