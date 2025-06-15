84b6b56583f0a822a93433961f5cb66c9c46a414
PLANETORIAN <daksh06shah2006@gmail.com>
2025-06-16 03:46:57 +0530

    feat: new responsive

--- src/components/prizes/prize.jsx
+++ src/components/prizes/prize.jsx
@@ -11,7 +11,7 @@ function Prize() {
         <SectionTitle title="PRIZES" />
       </div>
 
-      <div className="grid md:grid-cols-2 gap-8 px-4 pt-15 md:pt-40 md:max-w-[850px] xl:max-w-5xl w-full mx-5">
+      <div className="grid md:grid-cols-2 gap-8 max-w-[500px] px-10 sm:px-25 md:px-4 pt-15 md:pt-40 sm:max-w-[900px] xl:max-w-5xl w-full mx-5">
         <Gold />
         <Silver />
         <Bronze />
