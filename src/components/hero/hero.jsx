15e2c35bf11ceb93d166929ed81f6dea42522a2e
tilak-raaz <tilak4433@gmail.com>
2025-06-16 20:08:06 +0530

    fix: height of background

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -3,7 +3,7 @@ import Svg from "./ all-svg";
 import Hackodisha from "./hackodisha";
 const Hero = () => {
     return (
-         <div className=" bg-[#BC82FE] h-[1000px] sm:h-[1100px] lg:h-[850px]">
+         <div className=" bg-[#BC82FE] h-[940px] sm:h-[1100px] lg:h-[850px]">
             <div className="h-[750px] sm:h-[860px] lg:h-[800px] w-full  rounded-b-[100px]  sm:rounded-b-[200px]  lg:rounded-b-[300px] bg-[#D3AEFF]">
             <div className="h-[710px] sm:h-[820px] lg:h-[770px] w-full  rounded-b-[100px]  sm:rounded-b-[200px]   lg:rounded-b-[300px] bg-[#EFE7F7] ">
                 <Svg />
