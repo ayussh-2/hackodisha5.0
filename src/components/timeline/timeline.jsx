c9ea8a4ab6810f1d23f013095f777871aea60245
ayush <ayushatschool5@gmail.com>
2025-08-19 12:13:28 +0530

    iign schedule

--- src/components/timeline/timeline.jsx
+++ src/components/timeline/timeline.jsx
@@ -1,18 +1,24 @@
 "use client";
 import { useState } from "react";
+
 import Image from "next/image";
+
 import {
-    schedule,
-    dateMap,
-    topArrow,
     bottomArrow,
-    clockIcon,
-    reloadIcon,
+    dateMap,
+    iitgnSchedule,
+    schedule,
+    topArrow,
     webwizLogo,
 } from "@/config/timeline/index";
+
 import SectionTitle from "../shared/section-title";
+
 const Timeline = () => {
     const [activeDay, setActiveDay] = useState("Day 1");
+    const [isIITGN, setIsIITGN] = useState(false);
+
+    const currentSchedule = isIITGN ? iitgnSchedule : schedule;
 
     return (
         <div id="timeline">
@@ -54,24 +60,18 @@ const Timeline = () => {
 
                 <div className="rounded-[6rem] mt-24 border-[2px] border-black bg-[#D3AEFF] w-full ssm:w-[calc(100vw-4rem)]   ssm:max-w-7xl px-[clamp(1rem,4vw,2.44rem)] pt-[clamp(2rem,3.51rem,5vw)] pb-[clamp(2rem,3.51rem,5vw)]">
                     <div className="rounded-[6rem] border border-black bg-[#FAF5FF] w-full pt-8 px-6 pb-6 relative overflow-hidden">
-                        <div className="absolute top-4 left-8 z-10 hidden min-[619px]:block max-[580px]:left-4 max-[520px]:left-2 max-[500px]:left-1 max-[460px]:left-0">
-                            <Image
-                                src={clockIcon}
-                                alt="Clock Icon"
-                                width={90}
-                                height={90}
-                                className="w-[120px]"
-                            />
-                        </div>
-
-                        <div className="absolute bottom-6 right-3 z-10 max-[640px]:right-2 max-[600px]:right-1 max-[560px]:right-1 max-[520px]:right-1 max-[480px]:right-1 max-[440px]:right-1">
-                            <Image
-                                src={reloadIcon}
-                                alt="Reload Icon"
-                                width={36}
-                                height={36}
-                                className="max-[600px]:w-[32px] max-[600px]:h-[32px] max-[520px]:w-[28px] max-[520px]:h-[28px] max-[480px]:w-[24px] max-[480px]:h-[24px] max-[440px]:w-[20px] max-[440px]:h-[20px]"
-                            />
+                        {/* Toggle Button */}
+                        <div className="flex justify-end mb-4">
+                            <button
+                                onClick={() => setIsIITGN((prev) => !prev)}
+                                className={`px-6 font-archivo py-2 rounded-full font-bold border-2 border-black transition-all duration-300 ${
+                                    isIITGN
+                                        ? "bg-[#FFD32B] text-black"
+                                        : "bg-white text-black"
+                                }`}
+                            >
+                                {isIITGN ? "Online Schedule" : "IITGN Schedule"}
+                            </button>
                         </div>
 
                         <div className="flex justify-center mb-5 md:mb-10 lg:mb-20">
@@ -103,25 +103,24 @@ const Timeline = () => {
                         </div>
 
                         <div className="flex items-center justify-center relative ">
-                            {/* <div className="">
-                <Image src={DottedLine} alt="" />
-              </div> */}
                             <div className="flex-col relative w-full justify-center items-center flex  h-full">
-                                {schedule[activeDay].map((event, idx) => (
-                                    <div
-                                        key={idx}
-                                        className="relative z-10 mb-3 sm:mb-7 flex  items-center w-[100%] lg:w-[80%]"
-                                    >
-                                        <div className="flex items-center px-3 py-2 rounded-full border border-black bg-white w-full justify-start space-x-4 ml-2 flex-shrink-0 max-[640px]:px-2 max-[520px]:px-1.5 max-[480px]:px-1 max-[640px]:space-x-2 max-[480px]:space-x-1 max-[520px]:ml-1 max-[480px]:ml-0.5 ">
-                                            <div className="bg-[#FFD32B] text-[28px] max-sm:text-[14px] sm:text-md font-archivo font-semibold px-6 sm:px-8 py-1 rounded-full border border-black text-black max-[640px]:px-4 max-[520px]:px-3 max-[690px]:text-[18px] max-[740px]:text-[24px] max-[480px]:px-2 max-[640px]:text-xs max-[480px]:py-0.5">
-                                                {event.time}
+                                {currentSchedule[activeDay].map(
+                                    (event, idx) => (
+                                        <div
+                                            key={idx}
+                                            className="relative z-10 mb-3 sm:mb-7 flex  items-center w-[100%] lg:w-[80%]"
+                                        >
+                                            <div className="flex items-center px-3 py-2 rounded-full border border-black bg-white w-full justify-start space-x-4 ml-2 flex-shrink-0 max-[640px]:px-2 max-[520px]:px-1.5 max-[480px]:px-1 max-[640px]:space-x-2 max-[480px]:space-x-1 max-[520px]:ml-1 max-[480px]:ml-0.5 ">
+                                                <div className="bg-[#FFD32B] text-[28px] max-sm:text-[14px] sm:text-md font-archivo font-semibold px-6 sm:px-8 py-1 rounded-full border border-black text-black max-[640px]:px-4 max-[520px]:px-3 max-[690px]:text-[18px] max-[740px]:text-[24px] max-[480px]:px-2 max-[640px]:text-xs max-[480px]:py-0.5">
+                                                    {event.time}
+                                                </div>
+                                                <p className="text-[28px] max-sm:text-[14px] font-archivo font-semibold text-black whitespace-nowrap truncate max-[690px]:text-[18px] max-[740px]:text-[24px] max-[640px]:text-xs max-[480px]:text-[11px]">
+                                                    {event.label}
+                                                </p>
                                             </div>
-                                            <p className="text-[28px] max-sm:text-[14px] font-archivo font-semibold text-black whitespace-nowrap truncate max-[690px]:text-[18px] max-[740px]:text-[24px] max-[640px]:text-xs max-[480px]:text-[11px]">
-                                                {event.label}
-                                            </p>
                                         </div>
-                                    </div>
-                                ))}
+                                    )
+                                )}
                             </div>
                         </div>
                     </div>
--- src/config/timeline/index.js
+++ src/config/timeline/index.js
@@ -1,44 +1,68 @@
 export const schedule = {
-  "Day 1": [
-    { time: "7:30 AM", label: "Check-in starts" },
-    { time: "7:30 AM", label: "Doors open for participants" },
-    { time: "8:00 AM", label: "Opening Ceremony" },
-    { time: "8:30 AM", label: "Hacking Starts" },
-    { time: "9:00 AM", label: "Team Building Event" },
-    { time: "12:00 PM", label: "Lunch Break" },
-    { time: "3:30 PM", label: "Fun Event 1" },
-    { time: "5:00 PM", label: "Hacker Hangout" },
-    { time: "8:00 PM", label: "Fun Event 2" },
-  ],
-  "Day 2": [
-    { time: "12:00 AM", label: "Discord Catch-up Session" },
-    { time: "12:00 PM", label: "Lunch Break" },
-    { time: "2:00 PM", label: "Fun Event 3" },
-    { time: "6:00 PM", label: "Initial Submission Due Hacking Ends" },
-    { time: "9:00 PM", label: "(Submission hard deadline)" },
-    { time: "9:20 PM", label: "Closing Ceremony" },
-    { time: "10:00 PM", label: "Demo Starts" },
-    { time: "10:30 PM", label: "Demo Ends" },
-    { time: "11:00 PM", label: "Event Ends" },
-  ],
+    "Day 1": [
+        { time: "7:30 AM", label: "Check-in starts" },
+        { time: "7:30 AM", label: "Doors open for participants" },
+        { time: "8:00 AM", label: "Opening Ceremony" },
+        { time: "8:30 AM", label: "Hacking Starts" },
+        { time: "9:00 AM", label: "Team Building Event" },
+        { time: "12:00 PM", label: "Lunch Break" },
+        { time: "3:30 PM", label: "Fun Event 1" },
+        { time: "5:00 PM", label: "Hacker Hangout" },
+        { time: "8:00 PM", label: "Fun Event 2" },
+    ],
+    "Day 2": [
+        { time: "12:00 AM", label: "Discord Catch-up Session" },
+        { time: "12:00 PM", label: "Lunch Break" },
+        { time: "2:00 PM", label: "Fun Event 3" },
+        { time: "6:00 PM", label: "Initial Submission Due Hacking Ends" },
+        { time: "9:00 PM", label: "(Submission hard deadline)" },
+        { time: "9:20 PM", label: "Closing Ceremony" },
+        { time: "10:00 PM", label: "Demo Starts" },
+        { time: "10:30 PM", label: "Demo Ends" },
+        { time: "11:00 PM", label: "Event Ends" },
+    ],
 };
 
+export const iitgnSchedule = {
+    "Day 1": [
+        { time: "8:00 AM", label: "Opening Ceremony - Live Stream" },
+        { time: "9:00 AM", label: "Hacking Begins" },
+        { time: "12:00 PM", label: "Lunch Break" },
+        { time: "1:00 PM", label: "Hacking Continues" },
+        {
+            time: "3:00 PM",
+            label: "Online Mentoring / Offline Judging",
+        },
+        { time: "4:00 PM", label: "Hacking Continues" },
+        { time: "6:00 PM", label: "Snacks Break" },
+        { time: "7:00 PM", label: "Fun Event (if scheduled) / Hacking" },
+        { time: "9:00 PM", label: "Dinner" },
+        { time: "10:00 PM", label: "Overnight Hacking" },
+    ],
+    "Day 2": [
+        { time: "8:00 AM", label: "Breakfast" },
+        { time: "9:00 AM", label: "Hacking Continues" },
+        { time: "12:00 PM", label: "Lunch Break" },
+        { time: "1:00 PM", label: "Hacking Continues" },
+        { time: "4:00 PM", label: "Closing Ceremony" },
+    ],
+};
 export const dateMap = {
-  "Day 1": "SEPTEMBER 06 , 2025",
-  "Day 2": "SEPTEMBER 07 , 2025",
+    "Day 1": "SEPTEMBER 06 , 2025",
+    "Day 2": "SEPTEMBER 07 , 2025",
 };
 
 export const topArrow =
-  "https://res.cloudinary.com/dn0fpl7ks/image/upload/v1749840677/Arrow_01_w0sewq.svg";
+    "https://res.cloudinary.com/dn0fpl7ks/image/upload/v1749840677/Arrow_01_w0sewq.svg";
 export const bottomArrow =
-  "https://res.cloudinary.com/dn0fpl7ks/image/upload/v1749840676/Arrow_02_qya9jb.svg";
+    "https://res.cloudinary.com/dn0fpl7ks/image/upload/v1749840676/Arrow_02_qya9jb.svg";
 export const clockIcon =
-  "https://res.cloudinary.com/dn0fpl7ks/image/upload/v1749840677/clock_amvgts.svg";
+    "https://res.cloudinary.com/dn0fpl7ks/image/upload/v1749840677/clock_amvgts.svg";
 export const reloadIcon =
-  "https://res.cloudinary.com/dn0fpl7ks/image/upload/v1749840676/rewind_tcfsnc.svg";
+    "https://res.cloudinary.com/dn0fpl7ks/image/upload/v1749840676/rewind_tcfsnc.svg";
 export const timelineDot =
-  "https://res.cloudinary.com/dn0fpl7ks/image/upload/v1749840677/dot_jr2zjr.svg";
+    "https://res.cloudinary.com/dn0fpl7ks/image/upload/v1749840677/dot_jr2zjr.svg";
 export const dottedLine =
-  "https://res.cloudinary.com/dn0fpl7ks/image/upload/v1749840676/Line_48_rbxrhy.svg";
+    "https://res.cloudinary.com/dn0fpl7ks/image/upload/v1749840676/Line_48_rbxrhy.svg";
 export const webwizLogo =
-  "https://res.cloudinary.com/dn0fpl7ks/image/upload/v1750075008/webwiz_png_kfiwpt.png";
+    "https://res.cloudinary.com/dn0fpl7ks/image/upload/v1750075008/webwiz_png_kfiwpt.png";
